413ab0ed08f6a9d542acf4bc7dbe5a51b99189eb
Umesh7Dixit <umesh7dixit@gmail.com>
2023-07-01 22:59:04 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = ()=> {
-    mongoose.connect(process.env.MONGO_URI, { dbName:"backendapi"}).then(() => console.log("Shree Hare Radhe ,Database Connected")).catch((e)=> console.log("not connected Shree Hare Radhe"));
+    mongoose.connect(process.env.MONGO_URI, { dbName:"backendapi"}).then((c) => console.log(`Shree Hare Radhe ,Database Connected with ${c.connection.host}`)).catch((e)=> console.log("not connected Shree Hare Radhe"));
 };
